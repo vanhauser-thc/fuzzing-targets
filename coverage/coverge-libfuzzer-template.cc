8e28a8014a14e5f7487291b77bebb77e3bedaa01
van Hauser <vh@thc.org>
2020-04-21 20:29:17 +0200

    add libfuzzer coverage tools

---
+++ coverage/coverge-libfuzzer-template.cc
@@ -0,0 +1,27 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+//#include "libfoo/foo.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const unsigned char *data, size_t size) {
+
+  // do stuff
+
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  for (int i = 1; i < argc; i++) {
+    fprintf(stderr, "Running: %s (%d/%d)\n", argv[i], i, argc-1);
+    FILE *f = fopen(argv[i], "r");
+    fseek(f, 0, SEEK_END);
+    size_t len = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    unsigned char *buf = (unsigned char*)malloc(len);
+    size_t n_read = fread(buf, 1, len, f);
+    fclose(f);
+    LLVMFuzzerTestOneInput(buf, len);
+    free(buf);
+  }
+  fprintf(stderr, "Done.\n");
+}
